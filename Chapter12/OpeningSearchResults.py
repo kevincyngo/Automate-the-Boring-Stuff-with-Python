e8855eadb2f8db225236054c52b9b1ab5e517bd5
Kevin Ngo <ngokevin39@gmail.com>
2020-05-24 01:35:05 -0400

    implemented downloadXkcd

--- Chapter12/OpeningSearchResults.py
+++ Chapter12/OpeningSearchResults.py
@@ -38,39 +38,3 @@ if resp.status_code == 200:
     for i in range(numOpen):
         print('Opening', results[i]['link'])
         webbrowser.get(chrome_path).open(results[i]['link'])
-
-
-
-#
-# query = baseURL + ' '.join(sys.argv[1:])
-# headers = {"user-agent" : MOBILE_USER_AGENT}
-# res = requests.get(query, headers=headers)
-# try:
-#     res.raise_for_status()
-#     if res.status_code == 200:
-#
-#         soup = bs4.BeautifulSoup(res.content, 'html.parser')
-#         results = []
-#         for g in soup.find_all('div', class_='r'):
-#             anchors = g.find_all('a')
-#             if anchors:
-#                 link = anchors[0]['href']
-#                 title = g.find('h3').text
-#                 item = {
-#                     "title": title,
-#                     "link": link
-#                 }
-#             results.append(item)
-#             print(results)
-#     # numOpen = min(2, len(linkElems))
-#     # for i in range(numOpen):
-#     #     urlToOpen = baseURL + linkElems[i].get('href')
-#     #     print('Opening', urlToOpen)
-#     #     webbrowser.open(urlToOpen)
-# except Exception as exc:
-#     print("Error found: %s" % (exc))
-
-
-#retrieve top search result links
-
-#open browser tab for each result
