21415743cd75dc5ac068f3de5c3309d040e9857a
Kevin Ngo <ngokevin39@gmail.com>
2020-05-25 00:59:52 -0400

    Chapter 12 completed

--- Chapter12/automate_2048.py
+++ Chapter12/automate_2048.py
@@ -7,11 +7,17 @@ browser.get('https://gabrielecirulli.github.io/2048/')
 htmlElem = browser.find_element_by_tag_name('html')
 
 while True:
-    htmlElem.send_keys(Keys.DOWN)
+    htmlElem.send_keys(Keys.UP)
     htmlElem.send_keys(Keys.RIGHT)
+    htmlElem.send_keys(Keys.DOWN)
     htmlElem.send_keys(Keys.LEFT)
     try:
-        gameOverElem = browser.find_element_by_class_name('game-message game-over')
+        gameOverElem = browser.find_element_by_class_name('game-over')
+        score = browser.find_element_by_class_name('score-container').text
+
+        #score string is multi line, we want the first line (the score)
+        print("Score: %s" % score.partition('\n')[0])
+        browser.quit()
         break
     except:
         continue
